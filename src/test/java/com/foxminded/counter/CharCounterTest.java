a449715f35c9c228bae4da14d3ec2c325866365d
Dmitry Tebryaev <tebr5923@mail.ru>
2020-07-28 20:14:34 +0700

    add Test for clear map

--- src/test/java/com/foxminded/counter/CharCounterTest.java
+++ src/test/java/com/foxminded/counter/CharCounterTest.java
@@ -25,6 +25,27 @@ class CharCounterTest {
     }
 
     @Test
+    void count_shouldClearMap_whenCallCountTwoTimes() {
+        Map<Character, Integer> expected = new HashMapBuilder<Character>()
+                .add('H', 1)
+                .add('H', 1)
+                .add('e', 1)
+                .add('l', 3)
+                .add('o', 2)
+                .add(' ', 1)
+                .add('W', 1)
+                .add('r', 1)
+                .add('d', 1)
+                .add('!', 3)
+                .build();
+
+        CharCounter charCounter = new CharCounter();
+        charCounter.count("qwe");
+        Map<Character, Integer> actual = charCounter.count("Hello World!!!");
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void count_shouldReturnResult_whenStringIsRandom() {
         Map<Character, Integer> expected = new HashMapBuilder<Character>()
                 .add('H', 1)
