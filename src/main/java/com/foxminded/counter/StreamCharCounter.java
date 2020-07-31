5ea798fc415cf2857ae0d9ff54c61e937ae7a4f3
Dmitry Tebryaev <tebr5923@mail.ru>
2020-07-31 21:28:47 +0700

    add mapToObj

--- src/main/java/com/foxminded/counter/StreamCharCounter.java
+++ src/main/java/com/foxminded/counter/StreamCharCounter.java
@@ -1,6 +1,5 @@
 package com.foxminded.counter;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -23,20 +22,9 @@ public class StreamCharCounter implements Counter<Character> {
     }
 
     private Map<Character, Long> countChars(String string) {
-        Stream<Character> stream = toStream(string);
+        Stream<Character> stream = string.chars().mapToObj(ch -> (char) ch);
         Map<Character, Long> map;
         map = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return map;
-    }
-
-    private Stream<Character> toStream(String string) {
-        Character[] characters = new Character[string.length()];
-        //boxed
-        int i = 0;
-        for (char ch : string.toCharArray()) {
-            characters[i] = ch;
-            i++;
-        }
-        return Arrays.stream(characters);
     }
 }
